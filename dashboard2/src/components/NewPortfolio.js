a12fa63bf55c87d7bdecfad4f7c6e9050de4c47f
Seb <se.legoff@gmail.com>
2017-06-11 12:50:59 +0800

    Start working on forms

--- dashboard2/src/components/NewPortfolio.js
+++ dashboard2/src/components/NewPortfolio.js
@@ -1,18 +1,62 @@
 import React, { Component, PropTypes } from 'react'
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
+import { reduxForm, Field } from 'redux-form'
 
-export default class NewPortfolio extends Component {
+//http://www.material-ui.com/#/components/text-field
+//http://redux-form.com/6.5.0/examples/material-ui/
+
+
+const validate = values => {
+  const errors = {}
+  const requiredFields = [ 'name', 'desccription' ]
+  requiredFields.forEach(field => {
+    if (!values[ field ]) {
+      errors[ field ] = 'Required'
+    }
+  })
+  return errors
+}
+
+
+
+
+class NewPortfolio extends Component {
+   
+    handleFormSubmit({ name, description }) {
+        console.log(name, description);
+    }
 
     render() {
+        const { handleSubmit, fields: { name, description } } = this.props;
+
         return (
-            <div className="new-portfolio-div">
-                <TextField hintText="Name of your portoflio"  fullWidth={true}/>
-                <br />
-                <TextField hintText="Description of your portfolio" multiLine={true} rows={4} rowsMax={4}  fullWidth={true}/>
-                <br /><br /><br /><br />
-                <RaisedButton label="Validate" primary={true} />
-            </div>
+            <form onSubmit={handleSubmit}>
+                <div className="new-portfolio-div">
+                    <TextField {...name}
+                        hintText="Name of your portoflio"
+                        name="name"
+                        fullWidth={true} />
+                    <br />
+                    <TextField {...description}
+                        hintText="Description of your portfolio"
+                        name="description"
+                        multiLine={true}
+                        rows={4}
+                        rowsMax={4}
+                        fullWidth={true} />
+                    <br /><br /><br /><br />
+                    <RaisedButton
+                        type="submit"
+                        label="Validate"
+                        primary={true} />
+                </div>
+            </form>
         )
     }
 }
+
+export default reduxForm({
+    form: 'NewPortfolio',
+    validate,
+})(NewPortfolio)
